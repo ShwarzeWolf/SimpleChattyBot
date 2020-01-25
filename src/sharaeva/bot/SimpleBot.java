77c09013331f88ba040dd748213fec41ea0e8ae7
Kristin <1358365@DESKTOP-4FH9G41>
2020-01-25 13:04:40 +0300

    Stage 2. askName and guessAge functions added

--- src/sharaeva/bot/SimpleBot.java
+++ src/sharaeva/bot/SimpleBot.java
@@ -1,6 +1,7 @@
 package sharaeva.bot;
 
 import java.time.LocalDate;
+import java.util.Scanner;
 
 public class SimpleBot {
     private String name;
@@ -16,8 +17,34 @@ public class SimpleBot {
         System.out.println("I was created in " + this.birthDate.getYear() + ".");
     }
 
+    public void askName(){
+        System.out.println("Please, remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + userName + "!");
+    }
+
+    public void guessAge(){
+        System.out.println("Let me guess your age.\n" +
+                "Say me remainders of dividing your age by 3, 5 and 7.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        int reminder3 = scanner.nextInt();
+        int reminder5 = scanner.nextInt();
+        int reminder7 = scanner.nextInt();
+
+        int age = (reminder3 * 70 + reminder5 * 21 + reminder7 * 15) % 105;
+
+        System.out.println("Your age is " + age + "; that's a good time to start programming!");
+    }
+
     public static void main(String[] args) {
         SimpleBot bot = new SimpleBot("Ivan");
         bot.introduce();
+        bot.askName();
+        bot.guessAge();
     }
 }
